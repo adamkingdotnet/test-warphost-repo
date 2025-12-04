af578f6d3faf74ce5ded5fa778bc381fb384eec5
adamkingdotnet <adamking@hey.com>
2025-12-04 17:54:51 -0600

    Fix template literal syntax error

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(\`Server running on port \${PORT}\`);
+  console.log(`Server running on port ${PORT}`);
 });
